d3ab1830ab03507ff24dad4115769f1b0d957119
Shubham <127000297+shubham05055@users.noreply.github.com>
2026-01-25 18:43:37 +0530

    .

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function togglemenu(){
-    const menu= document.querySelector(".menu-links")
-    const icon= document.querySelector(".hamburger-icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+function togglemenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  const overlay = document.querySelector(".menu-overlay");
+
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+  overlay.classList.toggle("open");
 }
